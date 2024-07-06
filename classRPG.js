b3474adf5bace992381c9c33d9a8612cd0446360
Grandnork <paulolopestr@yahoo.com>
2024-07-06 02:12:15 -0300

    new examples and fixes

--- classRPG.js
+++ classRPG.js
@@ -4,18 +4,18 @@ class newCl{
     this.age = age
     this.job = type
 
-    if (type = "mage"){
+    if (type == "mage"){
         this.atktype = "magic"
-        }
-        else if (type = "warrior"){
+    }
+    else if (type == "warrior"){
         this.atktype = "sword"
-        }
-        else if(type = "monk"){
+    }
+    else if(type == "monk"){
         this.atktype = "martial arts"
-        }
-        else if (type = "ninja"){
+    }
+    else if (type == "ninja"){
         this.atktype = "shuriken"
-        }        
+    }        
     } 
     attack(){
         console.log(`the ${this.job} attacked using ${this.atktype}`)
@@ -23,8 +23,16 @@ class newCl{
 
 }
 
-let Paul123 = new newCl("Paul123", 21, "mage")
+let Paul123 = new newCl("Paul123", 30, "mage")
 Paul123.attack()
+let Paul124 = new newCl("Paul124", 18, "warrior")
+Paul124.attack()
+let Paul125 = new newCl("Paul125", 25, "monk")
+Paul125.attack()
+let Paul126 = new newCl("Paul126", 21, "ninja")
+Paul126.attack()
+let Paul127 = new newCl("Paul127", 45, "mage")
+Paul127.attack()
 
 
 
